feae162912475ae5ef624e9b279266141e5e691f
safsom <aryalsafal@gmail.com>
2018-06-09 09:10:50 +0630

    fixed horrible bug

--- shakti/include/sched.h
+++ shakti/include/sched.h
@@ -14,14 +14,13 @@ typedef struct {
 typedef struct {
     int attached;
     uint32_t cr3, ds;
-    uint32_t eip, esp;
-    uint32_t eax, ebx,
-             ecx, edx;
+    size_t nthreads;
     thread_t *threads;
     char name[20];
 } process_t;
 
 int sched_init();
+int sched_create_thread(int, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
 int sched_create(char *, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
 
 #endif
